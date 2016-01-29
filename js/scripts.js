87d115fbec3bc4880b7a2a2f593576a8a1f549fa
Anna Lawley <alawley1@gmail.com>
2016-01-29 08:29:38 -0800

    add function to calculate pizza cost

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,22 @@ function Pizza(pizzaSize, toppings, premiumToppings) {
   this.toppings = toppings;
   this.premiumToppings = premiumToppings;
 }
+
+Pizza.prototype.price = function() {
+  var pizzaPrice = 0
+  var toppingPrice = 0
+  if (this.pizzaSize === "Small") {
+    pizzaPrice = 12;
+  } else if (this.pizzaSize === "Medium") {
+    pizzaPrice = 14;
+  } else if (this.pizzaSize === "Large") {
+    pizzaPrice = 18;
+  } else {}
+  if (this.pizzaSize === "Small" || this.pizzaSize === "Medium") {
+    toppingPrice = this.toppings * 1 + this.premiumToppings * 2;
+  } else if (this.pizzaSize === "Large") {
+    toppingPrice = this.toppings * 1.5 + this.premiumToppings * 3;
+  }
+  pizzaPrice = pizzaPrice + toppingPrice
+  return pizzaPrice;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -5,4 +5,8 @@ describe('Pizza', function() {
     expect(testPizza.toppings).to.equal(3);
     expect(testPizza.premiumToppings).to.equal(2);
   });
+  it("adds the price method to all pizzas to calculate price", function() {
+    var testPizza = new Pizza("Large", 3, 2);
+    expect(testPizza.price()).to.equal(28.5);
+  });
 });
